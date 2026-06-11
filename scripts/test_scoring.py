88169b073aa8e36b38f38f35a66667185096a374
Aditya Diwane <55897634+adityadiwane@users.noreply.github.com>
2026-06-11 18:25:30 -0400

    Update scoring unit tests

--- scripts/test_scoring.py
+++ scripts/test_scoring.py
@@ -4,11 +4,17 @@ from scoring import Outcome, calculate_match_points, is_outcome_allowed
 
 
 class ScoringTests(unittest.TestCase):
-    def test_correct_pick_gets_three_points(self):
-        self.assertEqual(calculate_match_points(Outcome.HOME_WIN, Outcome.HOME_WIN), 3)
+    def test_correct_winner_pick_gets_two_points(self):
+        self.assertEqual(calculate_match_points(Outcome.HOME_WIN, Outcome.HOME_WIN), 2)
 
-    def test_wrong_pick_gets_zero_points(self):
+    def test_correct_draw_pick_gets_two_and_half_points(self):
+        self.assertEqual(calculate_match_points(Outcome.DRAW, Outcome.DRAW), 2.5)
+
+    def test_wrong_winner_pick_gets_zero_points(self):
         self.assertEqual(calculate_match_points(Outcome.AWAY_WIN, Outcome.HOME_WIN), 0)
+
+    def test_wrong_draw_pick_gets_zero_points(self):
+        self.assertEqual(calculate_match_points(Outcome.DRAW, Outcome.HOME_WIN), 0)
 
     def test_missing_actual_result_gets_zero_points(self):
         self.assertEqual(calculate_match_points(Outcome.AWAY_WIN, None), 0)
